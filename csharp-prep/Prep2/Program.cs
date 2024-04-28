63a3a683f8a42337908cfc96d69377ad5980ff06
claymore03 <113389977+claymore03@users.noreply.github.com>
2024-04-27 23:45:25 -0600

    Completed assignment.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,53 @@
+//Name: Clayton Redding
+//Date: 4/27/2024
+//Class: CSE 210
+//Instructor: Brother Gibbons
 using System;
 
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep2 World!");
+    {        
+        Console.Write("Enter your grade precentage (0 - 100)? ");
+        string grade = Console.ReadLine();
+
+        string letter = "";
+
+
+        if (Convert.ToInt32(grade) < 60) 
+        {
+            letter = "F";
+        } 
+        else if (Convert.ToInt32(grade) >= 60 && Convert.ToInt32(grade) < 70) 
+        {
+            letter = "D";
+        }
+        else if (Convert.ToInt32(grade) >= 70 && Convert.ToInt32(grade) < 80) 
+        {
+            letter = "C";
+        }
+        else if (Convert.ToInt32(grade) >= 80 && Convert.ToInt32(grade) < 90) 
+        {
+            letter = "B";
+        }
+        else if (Convert.ToInt32(grade) >= 90 && Convert.ToInt32(grade) < 101) 
+        {
+            letter = "A";
+        }    
+        else 
+        {
+            Console.Write("That's awkward.");
+        }
+
+        Console.WriteLine($"Your grade is: " + letter);
+
+        if (Convert.ToInt32(grade) < 70) 
+        {
+            Console.Write("You did not pass the course. Study harder next time. Don't give up!");
+        } 
+        else if (Convert.ToInt32(grade) >= 70 && Convert.ToInt32(grade) < 101) 
+        {
+            Console.Write("Congratulations! You passed the course.  Well done.");
+        }
     }
 }
