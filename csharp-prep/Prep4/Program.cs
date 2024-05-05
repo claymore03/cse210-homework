0f2250b84546cd6f41b6beb9a68ca0d43df96453
claymore03 <113389977+claymore03@users.noreply.github.com>
2024-05-05 00:11:26 -0600

    Completed core requirements.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,47 @@
+// Name: Clayton Redding
+// Instructor: Brother Gibbons
+// Class: CSE 210
+// Date: 05/04/2024
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numberList = new List<int>();
+
+        Console.Write("Add any number to the list! Type '0' when finished.");
+        int number = Convert.ToInt32(Console.ReadLine());
+        
+        while (number != 0)
+        {
+            numberList.Add(number);
+            Console.Write("Add another number? Type '0' when finished.");
+            number = Convert.ToInt32(Console.ReadLine());
+        } 
+        
+        int length = numberList.Count;
+        // Console.WriteLine("Length of list: " + length);
+        
+        int sum = 0;
+        foreach (int num in numberList)
+        {
+            sum += num;
+        }
+
+        int average = sum / length;
+
+        int max = numberList.Max();
+
+        Console.WriteLine("The sum of your list is: " + sum);
+
+        Console.WriteLine("The average is: " + average);
+
+        Console.WriteLine("The largest number is: " + max);
+        
+        // for(int i=0;i<numberList.Count;i++)
+        // {
+        // Console.WriteLine(numberList[i]);
+        // }
     }
 }
