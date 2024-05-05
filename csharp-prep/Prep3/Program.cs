cca007b0e75d29c432e4423034024a46712b2cea
claymore03 <113389977+claymore03@users.noreply.github.com>
2024-05-04 23:07:57 -0600

    Completed the assignment's core requirements.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("Welcome to 'Guess My Number'!");
+
+        Random rd = new Random();
+
+        int rand_num = rd.Next(1,100);
+
+        Console.WriteLine("What is the magic number? " + rand_num);
+
+        Console.Write("Enter your guess (Pick a number between 1 and 100)");
+        string guess = Console.ReadLine();
+
+        while (Convert.ToInt32(guess) != rand_num) 
+        {
+
+            Console.WriteLine("You guessed " + guess);
+
+           if (Convert.ToInt32(guess) < rand_num) 
+            {
+                Console.WriteLine("Higher");
+            } 
+            else if (Convert.ToInt32(guess) > rand_num) 
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("That's awkward");
+            }
+
+            Console.WriteLine("Try again! (Pick a number between 1 and 100)");
+            guess = Console.ReadLine(); 
+            
+        }
+
+        Console.WriteLine("Congratulations! You guessed the magic number!");
+        
+        
     }
 }
