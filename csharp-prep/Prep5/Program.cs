8d1493d3f97d69d9b718694feaed9c1ca57d0455
claymore03 <113389977+claymore03@users.noreply.github.com>
2024-05-05 01:00:33 -0600

    Completed core requirements.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using Microsoft.VisualBasic;
 
 class Program
 {
+    static void DisplayWelcome() 
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        string favNum = Console.ReadLine();
+        return Convert.ToInt32(favNum);
+    }
+
+    static int SquareNumber(int x) 
+    {
+        return x * x;
+    }
+
+    static void DisplayResults(string name, string squaredNum) 
+    {
+        Console.WriteLine(name + ", the square of your number is " + squaredNum + ".");
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        int squared = SquareNumber(number);
+        DisplayResults(name, Convert.ToString(squared));
     }
 }
