c087bfab5c470c825621033f8eb04b5a7929c7eb
claymore03 <113389977+claymore03@users.noreply.github.com>
2024-06-25 20:34:13 -0600

    Updated Learning 05 assignment

---
+++ prepare/Learning05/Circle.cs
@@ -0,0 +1,18 @@
+public class Circle : Shape
+{
+    private double _radius;
+
+    public double GetRadius()
+    {
+        return _radius;
+    }
+
+    public void SetRadius(double radius)
+    {
+        _radius = radius;
+    }
+    public override double GetArea()
+    {
+        return _radius * _radius * Math.PI;
+    }
+}
--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -5,5 +5,29 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning05 World!");
+
+        Square square = new Square();
+        square.SetColor("Blue");
+        square.SetSide(4);
+
+        Rectangle rectangle = new Rectangle();
+        rectangle.SetColor("Red");
+        rectangle.SetLength(8);
+        rectangle.SetWidth(4);
+
+        Circle circle = new Circle();
+        circle.SetColor("Green");
+        circle.SetRadius(5);
+
+        List<Shape> shape = new List<Shape>();
+        shape.Add(square);
+        shape.Add(rectangle);
+        shape.Add(circle);
+
+        foreach (Shape s in shape)
+        {
+            double area = s.GetArea();
+            s.GetColor();
+        }
     }
 }
