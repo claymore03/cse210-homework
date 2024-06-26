79b494f6720aa63b115075015c05573547e82a79
claymore03 <113389977+claymore03@users.noreply.github.com>
2024-06-25 21:02:43 -0600

    Finished project

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,24 +1,30 @@
+//Name: Clayton Redding
+//Course Title: CSE 210
+//Professor: Brother Gibbons
+//Date: 25 June 2024
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
-
+        //New instance of Square class
         Square square = new Square();
         square.SetColor("Blue");
         square.SetSide(4);
 
+        //New instance of Rectangle class
         Rectangle rectangle = new Rectangle();
         rectangle.SetColor("Red");
         rectangle.SetLength(8);
         rectangle.SetWidth(4);
 
+        //New instance of Circle class
         Circle circle = new Circle();
         circle.SetColor("Green");
         circle.SetRadius(5);
 
+        //List of shapes
         List<Shape> shape = new List<Shape>();
         shape.Add(square);
         shape.Add(rectangle);
@@ -26,8 +32,9 @@ class Program
 
         foreach (Shape s in shape)
         {
-            double area = s.GetArea();
-            s.GetColor();
+            Console.WriteLine($"The {s.GetColor()} shape has an area of {s.GetArea()}.");
+            Console.WriteLine();
+            
         }
     }
 }
